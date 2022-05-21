be6c9b619718b43c376ce1f8287bdc7452a849eb
RONY VARGHESE <RONY VARGHESE@MSI>
2022-05-21 09:21:59 -0800

    Add project files.

---
+++ Gymzilla/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gymzilla.Models
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
