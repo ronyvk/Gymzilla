aadf33d15824c239a2eea36ab25413dcc83d8a16
RONY VARGHESE <RONY VARGHESE@MSI>
2022-05-21 09:21:59 -0800

    Add project files.

---
+++ Gymzilla/Controllers/BrandController.cs
@@ -0,0 +1,32 @@
+﻿using Gymzilla.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gymzilla.Controllers
+{
+    public class BrandController : Controller
+    {
+        public IActionResult Index()
+        {
+            var brands = new List<Brand>();
+            for(var i = 1; i < 11; i++)
+            {
+               brands.Add(new Brand { Name = "Brand" + i.ToString() });
+            }
+            return View(brands);
+        }
+        public IActionResult ShopByBrand(string brand)
+        {
+            if(brand==null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewData["brand"] = brand;
+
+            return View();
+        }
+    }
+}
